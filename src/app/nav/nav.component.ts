b59e3e8738ab119d23bf2c66ee8bf051ac51ef68
ibrahimrahhal <ibrahim.rahhal3636@gmail.com>
2018-11-16 04:56:15 -0800

    header

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+
+export class NavComponent implements OnInit {
+
+  constructor() {
+
+
+ }
+
+  ngOnInit() {
+
+  }
+
+   list:string[]=["home","tutorials","contact","about us"];
+
+
+
+}
